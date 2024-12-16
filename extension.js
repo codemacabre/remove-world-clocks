935cfe28f61d85b85947a2929ed4bd0a9e1aa8e3
Myles Lewando <myles.lewando@opendataservices.coop>
2024-12-16 17:16:59 +0000

    Use signal connections to toggle _clocksItem

--- extension.js
+++ extension.js
@@ -1,12 +1,34 @@
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-const DateMenu = Main.panel.statusArea.dateMenu
-
 export default class RemoveWorldClocks {
+  #state = {};
+
   enable() {
-    DateMenu._clocksItem.hide();
+    const clocksItem = this._main.panel.statusArea.dateMenu._clocksItem;
+
+    if (this.#state.clocksItemUpdate) {
+      clocksItem._update = this.#state.clocksItemUpdate;
+      delete this.#state.clocksItemUpdate;
+
+      if (this._clocksItemSignal) {
+        clocksItem.disconnect(this._clocksItemSignal);
+        delete this._clocksItemSignal;
+      }
+
+      clocksItem._update();
+    }
   }
 
   disable() {
-    DateMenu._clocksItem.show();
+    const clocksItem = this._main.panel.statusArea.dateMenu._clocksItem;
+
+    if (!this.#state.clocksItemUpdate) {
+      this.#state.clocksItemUpdate = clocksItem._update;
+      clocksItem._update = () => {
+        clocksItem.visible = false;
+      }
+
+      this._clocksItemSignal = clocksItem.connect('show', clocksItem._update);
+    }
+
+    clocksItem._update();
   }
 }
